b6f3d069dbb558a7fdb4cb15df4b8c298c7f3276
Ali Sweid <asweid657@gmail.com>
2025-07-17 14:01:55 +0300

    efef

---
+++ lib/constant/constant.dart
@@ -0,0 +1,6 @@
+// Fill in the app ID obtained from the Agora console
+const appId = "650944019d314e64bd169f963c1c674e";
+// Fill in the temporary token generated from Agora Console
+const token = "007eJxTYJhZcfZLd5Zax+/EW24pxoscvAV7MhLUbTPtal4s/J+/MFiBwczUwNLExMDQMsXY0CTVzCQpxdDMMs3SzDjZMNnM3CT129WKjIZARobFIWEMjFAI4rMwlKQWlzAwAAA4fB89";
+// Fill in the channel name you used to generate the token
+const channel = "test";
